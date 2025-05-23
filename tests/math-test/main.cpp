5341164c24e7ea3d14aa7cbd091b3d72722e6565
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-05-23 22:47:36 +0700

    wip: vector operations conformance draft

--- tests/math-test/main.cpp
+++ tests/math-test/main.cpp
@@ -8,8 +8,6 @@
 #include "vector_operations.hpp"
 
 int main(int argc, char* argv[]) {
-    QCE::vector_addition_def(nullptr, 0, nullptr);
-
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
--- tests/math-test/vector_operations_conformance.cpp
+++ tests/math-test/vector_operations_conformance.cpp
@@ -5,3 +5,27 @@
 
 // TODO: common-utils: test_utils::conformance_test
 
+#include <gtest/gtest.h>
+
+#include <cu/file-utils.hpp>
+
+#include "vector_operations.hpp"
+
+TEST(VectorOperationsConformance, Addition) {
+    static constexpr auto INPUT_FILE = QCE_TEST_DATA_PATH "/16000000_float32.bin";
+    static constexpr auto OUTPUT_FILE = QCE_TEST_DATA_PATH "/vector4_float32_sum.bin";
+
+    auto input_data = CU::load_data_from_file<float>(INPUT_FILE);
+    ASSERT_FALSE(input_data.empty());
+
+    auto output_data = CU::load_data_from_file<float>(OUTPUT_FILE);
+    ASSERT_FALSE(output_data.empty());
+
+    std::vector<float> result_data(output_data.size());
+    QCE::vector_addition_def(input_data.data(), input_data.size(), result_data.data());
+
+    for (unsigned i = 0; i < output_data.size(); i++) {
+        ASSERT_EQ(output_data[i], result_data[i]);
+    }
+}
+
