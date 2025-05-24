36a820420986e07cd6fdded77922e01e4db9ee10
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-05-24 18:10:09 +0700

    use simd-utils

--- include/qce/math.hpp
+++ include/qce/math.hpp
@@ -5,17 +5,5 @@
 
 #pragma once
 
-// TODO: common-utils macro
-// select implementation
-#if defined(CU_COMPILE_UNIT_SSE2)
-#  include <qce/math/vector_sse2.hpp>
-#  include <qce/math/matrix_sse2.hpp>
-#elif defined(CU_COMPILE_UNIT_AVX512)
-#  include <qce/math/vector_avx512.hpp> // TODO: do we need it?
-#  include <qce/math/matrix_avx512.hpp>
-#else // CU_COMPILE_UNIT_DEF or just default implementation
-#  include <qce/math/vector_def.hpp>
-#  include <qce/math/matrix_def.hpp>
-#endif
-
+#include <qce/vector.hpp>
 
--- tests/math-test/main.cpp
+++ tests/math-test/main.cpp
@@ -5,8 +5,6 @@
 
 #include <gtest/gtest.h>
 
-#include "vector_operations.hpp"
-
 int main(int argc, char* argv[]) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
