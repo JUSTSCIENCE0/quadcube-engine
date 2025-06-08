74ae1843a600d540be3f8684b6da717a6af3459b
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-06-08 22:12:33 +0700

    add performance test

---
+++ tests/math-test/vector_operations_performance.cpp
@@ -0,0 +1,42 @@
+// Copyright (c) 2025, Yakov Usoltsev
+// Email: yakovmen62@gmail.com
+//
+// License: MIT
+
+#define CU_PRINT_PERFORMANCE_TEST_RESULT
+
+#include <cu/test-utils.hpp>
+
+#include "vector_operations.hpp"
+
+CU_PERFORMANCE_TEST_SIMD(
+    VectorAddition,
+    QCE_TEST_DATA_PATH,
+    "16000000_float32.bin",
+    QCE::vector_addition,
+    (def, sse2)
+)
+
+CU_PERFORMANCE_TEST_SIMD(
+    VectorSubtraction,
+    QCE_TEST_DATA_PATH,
+    "16000000_float32.bin",
+    QCE::vector_subtraction,
+    (def, sse2)
+)
+
+CU_PERFORMANCE_TEST_SIMD(
+    VectorMultiplication,
+    QCE_TEST_DATA_PATH,
+    "16000000_float32.bin",
+    QCE::vector_multiplication,
+    (def, sse2)
+)
+
+CU_PERFORMANCE_TEST_SIMD(
+    VectorDivision,
+    QCE_TEST_DATA_PATH,
+    "16000000_float32.bin",
+    QCE::vector_division,
+    (def, sse2)
+)
