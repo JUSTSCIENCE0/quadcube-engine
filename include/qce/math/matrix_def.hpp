39584cc65147129135280dcde67ccec2d5843a43
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-06-19 21:43:16 +0700

    matrix default base operations

--- include/qce/math/matrix_def.hpp
+++ include/qce/math/matrix_def.hpp
@@ -24,11 +24,6 @@ namespace QCE {
         float x4, y4, z4, w4;
     };
 
-    static inline matrix& operator+=(      matrix& lhs, const matrix& rhs) noexcept;
-    static inline matrix  operator+ (const matrix& lhs, const matrix& rhs) noexcept;
-    static inline matrix& operator-=(      matrix& lhs, const matrix& rhs) noexcept;
-    static inline matrix  operator- (const matrix& lhs, const matrix& rhs) noexcept;
-
     static inline matrix matrix_init(
             float x1, float y1, float z1, float w1,
             float x2, float y2, float z2, float w2,
@@ -71,6 +66,125 @@ namespace QCE {
     }
 
     static inline matrix matrix_zero() noexcept {
+        return {};
+    }
+
+    static inline matrix operator+(const matrix& lhs, const matrix& rhs) noexcept {
+        return {
+            lhs.x1 + rhs.x1, lhs.y1 + rhs.y1, lhs.z1 + rhs.z1, lhs.w1 + rhs.w1,
+            lhs.x2 + rhs.x2, lhs.y2 + rhs.y2, lhs.z2 + rhs.z2, lhs.w2 + rhs.w2,
+            lhs.x3 + rhs.x3, lhs.y3 + rhs.y3, lhs.z3 + rhs.z3, lhs.w3 + rhs.w3,
+            lhs.x4 + rhs.x4, lhs.y4 + rhs.y4, lhs.z4 + rhs.z4, lhs.w4 + rhs.w4,
+        };
+    }
+
+    static inline matrix& operator+=(matrix& lhs, const matrix& rhs) noexcept {
+        lhs = lhs + rhs;
+        return lhs;
+    }
+
+    static inline matrix operator-(const matrix& lhs, const matrix& rhs) noexcept {
+        return {
+            lhs.x1 - rhs.x1, lhs.y1 - rhs.y1, lhs.z1 - rhs.z1, lhs.w1 - rhs.w1,
+            lhs.x2 - rhs.x2, lhs.y2 - rhs.y2, lhs.z2 - rhs.z2, lhs.w2 - rhs.w2,
+            lhs.x3 - rhs.x3, lhs.y3 - rhs.y3, lhs.z3 - rhs.z3, lhs.w3 - rhs.w3,
+            lhs.x4 - rhs.x4, lhs.y4 - rhs.y4, lhs.z4 - rhs.z4, lhs.w4 - rhs.w4,
+        };
+    }
+
+    static inline matrix& operator-=(matrix& lhs, const matrix& rhs) noexcept {
+        lhs = lhs - rhs;
+        return lhs;
+    }
+
+    static inline matrix operator*(const matrix& lhs, const matrix& rhs) noexcept {
+        auto v1 = vector_init(lhs.x1, lhs.y1, lhs.w1, lhs.z1);
+        auto v2 = vector_init(lhs.x2, lhs.y2, lhs.w2, lhs.z2);
+        auto v3 = vector_init(lhs.x3, lhs.y3, lhs.w3, lhs.z3);
+        auto v4 = vector_init(lhs.x4, lhs.y4, lhs.w4, lhs.z4);
+
+        auto vx = vector_init(rhs.x1, rhs.x2, rhs.x3, rhs.x4);
+        auto vy = vector_init(rhs.y1, rhs.y2, rhs.y3, rhs.y4);
+        auto vz = vector_init(rhs.z1, rhs.z2, rhs.z3, rhs.z4);
+        auto vw = vector_init(rhs.w1, rhs.w2, rhs.w3, rhs.w4);
+
+        return {
+            .x1 = vector_dot_product(v1, vx),
+            .y1 = vector_dot_product(v1, vy),
+            .z1 = vector_dot_product(v1, vz),
+            .w1 = vector_dot_product(v1, vw),
+
+            .x2 = vector_dot_product(v2, vx),
+            .y2 = vector_dot_product(v2, vy),
+            .z2 = vector_dot_product(v2, vz),
+            .w2 = vector_dot_product(v2, vw),
+
+            .x3 = vector_dot_product(v3, vx),
+            .y3 = vector_dot_product(v3, vy),
+            .z3 = vector_dot_product(v3, vz),
+            .w3 = vector_dot_product(v3, vw),
+
+            .x4 = vector_dot_product(v4, vx),
+            .y4 = vector_dot_product(v4, vy),
+            .z4 = vector_dot_product(v4, vz),
+            .w4 = vector_dot_product(v4, vw)
+        };
+    }
+
+    static inline matrix& operator*=(matrix& lhs, const matrix& rhs) noexcept {
+        lhs = lhs * rhs;
+        return lhs;
+    }
+
+    static inline vector operator*(const vector& lhs, const matrix& rhs) noexcept {
+        auto vx = vector_init(rhs.x1, rhs.x2, rhs.x3, rhs.x4);
+        auto vy = vector_init(rhs.y1, rhs.y2, rhs.y3, rhs.y4);
+        auto vz = vector_init(rhs.z1, rhs.z2, rhs.z3, rhs.z4);
+        auto vw = vector_init(rhs.w1, rhs.w2, rhs.w3, rhs.w4);
+
+        return vector_init(
+            vector_dot_product(lhs, vx),
+            vector_dot_product(lhs, vy),
+            vector_dot_product(lhs, vz),
+            vector_dot_product(lhs, vw)
+        );
+    }
+
+    static inline vector& operator*=(vector& lhs, const matrix& rhs) noexcept {
+        lhs = lhs * rhs;
+        return lhs;
+    }
+
+    static inline vector operator*(const matrix& lhs, const vector& rhs) noexcept {
+        auto v1 = vector_init(lhs.x1, lhs.y1, lhs.w1, lhs.z1);
+        auto v2 = vector_init(lhs.x2, lhs.y2, lhs.w2, lhs.z2);
+        auto v3 = vector_init(lhs.x3, lhs.y3, lhs.w3, lhs.z3);
+        auto v4 = vector_init(lhs.x4, lhs.y4, lhs.w4, lhs.z4);
+
+        return vector_init(
+            vector_dot_product(v1, rhs),
+            vector_dot_product(v2, rhs),
+            vector_dot_product(v3, rhs),
+            vector_dot_product(v4, rhs)
+        );
+    }
+
+    static inline matrix matrix_transpose(const matrix& mtx) noexcept {
+        return {
+            mtx.x1, mtx.x2, mtx.x3, mtx.x4,
+            mtx.y1, mtx.y2, mtx.y3, mtx.y4,
+            mtx.z1, mtx.z2, mtx.z3, mtx.z4,
+            mtx.w1, mtx.w2, mtx.w3, mtx.w4
+        };
+    }
+
+    static inline float matrix_determinant(const matrix& mtx) noexcept {
+        // TODO
+        return 0.0f;
+    }
+
+    static inline matrix matrix_inverse(const matrix& mtx) noexcept {
+        // TODO
         return {};
     }
 }
