e21b0e87b5d242073b5570f57f5000c9718db4ff
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-06-23 22:11:55 +0700

    add matrix operations performance tests

---
+++ tests/math-test/matrix_operations_performance.cpp
@@ -0,0 +1,72 @@
+// Copyright (c) 2025, Yakov Usoltsev
+// Email: yakovmen62@gmail.com
+//
+// License: MIT
+
+#include <cu/test-utils.hpp>
+
+#include "matrix_operations.hpp"
+
+CU_PERFORMANCE_TEST_SIMD(
+    MatrixAddition,
+    QCE_TEST_DATA_PATH,
+    "16000000_float32.bin",
+    QCE::matrix_addition,
+    (def)
+)
+
+CU_PERFORMANCE_TEST_SIMD(
+    MatrixSubtraction,
+    QCE_TEST_DATA_PATH,
+    "16000000_float32.bin",
+    QCE::matrix_subtraction,
+    (def)
+)
+
+CU_PERFORMANCE_TEST_SIMD(
+    MatrixMultiplication,
+    QCE_TEST_DATA_PATH,
+    "16000000_float32.bin",
+    QCE::matrix_multiplication,
+    (def)
+)
+
+CU_PERFORMANCE_TEST_SIMD(
+    VectorMatrixMultiplication,
+    QCE_TEST_DATA_PATH,
+    "16000000_float32.bin",
+    QCE::vector_matrix_multiplication,
+    (def)
+)
+
+CU_PERFORMANCE_TEST_SIMD(
+    MatrixVectorMultiplication,
+    QCE_TEST_DATA_PATH,
+    "16000000_float32.bin",
+    QCE::matrix_vector_multiplication,
+    (def)
+)
+
+CU_PERFORMANCE_TEST_SIMD(
+    MatrixTranspose,
+    QCE_TEST_DATA_PATH,
+    "16000000_float32.bin",
+    QCE::matrix_calc_transpose,
+    (def)
+)
+
+CU_PERFORMANCE_TEST_SIMD(
+    MatrixDeterminant,
+    QCE_TEST_DATA_PATH,
+    "16000000_float32.bin",
+    QCE::matrix_calc_determinant,
+    (def)
+)
+
+CU_PERFORMANCE_TEST_SIMD(
+    MatrixInverse,
+    QCE_TEST_DATA_PATH,
+    "16000000_float32.bin",
+    QCE::matrix_calc_inverse,
+    (def)
+)
