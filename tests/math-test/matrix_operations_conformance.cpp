f7ba1f433e18fe05e59eec4c0d694e9269cd18a7
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-07-04 21:35:44 +0700

    tiny test dataset support

--- tests/math-test/matrix_operations_conformance.cpp
+++ tests/math-test/matrix_operations_conformance.cpp
@@ -3,6 +3,14 @@
 //
 // License: MIT
 
+#define USE_TINY_DATASET // TODO: CMake implementation
+
+#ifdef USE_TINY_DATASET
+#  define SOURCE_FILE "500000_float32.bin"
+#else
+#  define SOURCE_FILE "4000000_float32.bin"
+#endif
+
 #include <cu/test-utils.hpp>
 
 #include "matrix_operations.hpp"
@@ -10,7 +18,7 @@
 CU_CONFORMANCE_TEST_SIMD(
     MatrixAddition,
     QCE_TEST_DATA_PATH,
-    "4000000_float32.bin",
+    SOURCE_FILE,
     "matrix4x4_float32_sum.bin",
     QCE::matrix_addition,
     (def, sse2)
@@ -19,7 +27,7 @@ CU_CONFORMANCE_TEST_SIMD(
 CU_CONFORMANCE_TEST_SIMD(
     MatrixSubtraction,
     QCE_TEST_DATA_PATH,
-    "4000000_float32.bin",
+    SOURCE_FILE,
     "matrix4x4_float32_diff.bin",
     QCE::matrix_subtraction,
     (def, sse2)
@@ -28,7 +36,7 @@ CU_CONFORMANCE_TEST_SIMD(
 CU_CONFORMANCE_TEST_SIMD(
     MatrixMultiplication,
     QCE_TEST_DATA_PATH,
-    "4000000_float32.bin",
+    SOURCE_FILE,
     "matrix4x4_float32_mul.bin",
     QCE::matrix_multiplication,
     (def, sse2)
@@ -37,7 +45,7 @@ CU_CONFORMANCE_TEST_SIMD(
 CU_CONFORMANCE_TEST_SIMD(
     VectorMatrixMultiplication,
     QCE_TEST_DATA_PATH,
-    "4000000_float32.bin",
+    SOURCE_FILE,
     "matrix4x4_float32_vector_matrix_mul.bin",
     QCE::vector_matrix_multiplication,
     (def, sse2)
@@ -46,7 +54,7 @@ CU_CONFORMANCE_TEST_SIMD(
 CU_CONFORMANCE_TEST_SIMD(
     MatrixVectorMultiplication,
     QCE_TEST_DATA_PATH,
-    "4000000_float32.bin",
+    SOURCE_FILE,
     "matrix4x4_float32_matrix_vector_mul.bin",
     QCE::matrix_vector_multiplication,
     (def, sse2)
@@ -55,7 +63,7 @@ CU_CONFORMANCE_TEST_SIMD(
 CU_CONFORMANCE_TEST_SIMD(
     MatrixTranspose,
     QCE_TEST_DATA_PATH,
-    "4000000_float32.bin",
+    SOURCE_FILE,
     "matrix4x4_float32_transpose.bin",
     QCE::matrix_calc_transpose,
     (def, sse2)
@@ -64,7 +72,7 @@ CU_CONFORMANCE_TEST_SIMD(
 CU_CONFORMANCE_TEST_SIMD(
     MatrixDeterminant,
     QCE_TEST_DATA_PATH,
-    "4000000_float32.bin",
+    SOURCE_FILE,
     "matrix4x4_float32_det.bin",
     QCE::matrix_calc_determinant,
     (def, sse2)
@@ -73,7 +81,7 @@ CU_CONFORMANCE_TEST_SIMD(
 CU_CONFORMANCE_TEST_SIMD(
     MatrixInverse,
     QCE_TEST_DATA_PATH,
-    "4000000_float32.bin",
+    SOURCE_FILE,
     "matrix4x4_float32_invert.bin",
     QCE::matrix_calc_inverse,
     (def, sse2)
--- tests/math-test/vector_operations_conformance.cpp
+++ tests/math-test/vector_operations_conformance.cpp
@@ -3,6 +3,14 @@
 //
 // License: MIT
 
+#define USE_TINY_DATASET // TODO: CMake implementation
+
+#ifdef USE_TINY_DATASET
+#  define SOURCE_FILE "500000_float32.bin"
+#else
+#  define SOURCE_FILE "4000000_float32.bin"
+#endif
+
 #include <cu/test-utils.hpp>
 
 #include "vector_operations.hpp"
@@ -10,7 +18,7 @@
 CU_CONFORMANCE_TEST_SIMD(
     VectorAddition,
     QCE_TEST_DATA_PATH,
-    "4000000_float32.bin",
+    SOURCE_FILE,
     "vector4_float32_sum.bin",
     QCE::vector_addition,
     ( def, sse2 )
@@ -19,7 +27,7 @@ CU_CONFORMANCE_TEST_SIMD(
 CU_CONFORMANCE_TEST_SIMD(
     VectorSubtraction,
     QCE_TEST_DATA_PATH,
-    "4000000_float32.bin",
+    SOURCE_FILE,
     "vector4_float32_diff.bin",
     QCE::vector_subtraction,
     ( def, sse2 )
@@ -28,7 +36,7 @@ CU_CONFORMANCE_TEST_SIMD(
 CU_CONFORMANCE_TEST_SIMD(
     VectorMultiplication,
     QCE_TEST_DATA_PATH,
-    "4000000_float32.bin",
+    SOURCE_FILE,
     "vector4_float32_mul.bin",
     QCE::vector_multiplication,
     ( def, sse2 )
@@ -37,7 +45,7 @@ CU_CONFORMANCE_TEST_SIMD(
 CU_CONFORMANCE_TEST_SIMD(
     VectorDivision,
     QCE_TEST_DATA_PATH,
-    "4000000_float32.bin",
+    SOURCE_FILE,
     "vector4_float32_div.bin",
     QCE::vector_division,
     ( def, sse2 )
@@ -46,7 +54,7 @@ CU_CONFORMANCE_TEST_SIMD(
 CU_CONFORMANCE_TEST_SIMD(
     VectorScalarMultiplication,
     QCE_TEST_DATA_PATH,
-    "4000000_float32.bin",
+    SOURCE_FILE,
     "vector4_float32_scalar_mul.bin",
     QCE::vector_scalar_multiplication,
     (def, sse2)
@@ -55,7 +63,7 @@ CU_CONFORMANCE_TEST_SIMD(
 CU_CONFORMANCE_TEST_SIMD(
     VectorScalarDivision,
     QCE_TEST_DATA_PATH,
-    "4000000_float32.bin",
+    SOURCE_FILE,
     "vector4_float32_scalar_div.bin",
     QCE::vector_scalar_division,
     (def, sse2)
@@ -64,7 +72,7 @@ CU_CONFORMANCE_TEST_SIMD(
 CU_CONFORMANCE_TEST_SIMD(
     VectorLength,
     QCE_TEST_DATA_PATH,
-    "4000000_float32.bin",
+    SOURCE_FILE,
     "vector4_float32_length.bin",
     QCE::vector_calc_length,
     (def, sse2)
@@ -73,7 +81,7 @@ CU_CONFORMANCE_TEST_SIMD(
 CU_CONFORMANCE_TEST_SIMD(
     VectorNormalization,
     QCE_TEST_DATA_PATH,
-    "4000000_float32.bin",
+    SOURCE_FILE,
     "vector4_float32_normalize.bin",
     QCE::vector_calc_normalized,
     (def, sse2)
@@ -82,7 +90,7 @@ CU_CONFORMANCE_TEST_SIMD(
 CU_CONFORMANCE_TEST_SIMD(
     VectorDotProduct,
     QCE_TEST_DATA_PATH,
-    "4000000_float32.bin",
+    SOURCE_FILE,
     "vector4_float32_dot_product.bin",
     QCE::vector_calc_dot_product,
     (def, sse2)
@@ -91,7 +99,7 @@ CU_CONFORMANCE_TEST_SIMD(
 CU_CONFORMANCE_TEST_SIMD(
     VectorCrossProduct,
     QCE_TEST_DATA_PATH,
-    "4000000_float32.bin",
+    SOURCE_FILE,
     "vector4_float32_cross_product.bin",
     QCE::vector_calc_cross_product,
     (def, sse2)
@@ -100,7 +108,7 @@ CU_CONFORMANCE_TEST_SIMD(
 CU_CONFORMANCE_TEST_SIMD(
     VectorCompare,
     QCE_TEST_DATA_PATH,
-    "4000000_float32.bin",
+    SOURCE_FILE,
     "vector4_float32_zero.bin",
     QCE::vector_equality,
     (def, sse2)
