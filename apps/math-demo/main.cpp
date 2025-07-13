0a112cad9e0a465e490d64ea3e35c528a48ed6e0
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-07-13 20:56:23 +0700

    clear math-demo

--- apps/math-demo/main.cpp
+++ apps/math-demo/main.cpp
@@ -4,43 +4,42 @@
 // License: MIT
 
 //#include <qce/math.hpp>
-#include <qce/math/matrix_avx512.hpp>
-
-using namespace QCE;
+//#include <qce/math/matrix_avx512.hpp>
+//using namespace QCE;
 
 int main() {
-    float mem[] = { 4.0f, 5.0f, 6.0f, 7.0f };
+    // float mem[] = { 4.0f, 5.0f, 6.0f, 7.0f };
 
-    auto v1 = vector_init(0.0f, 1.0f, 2.0f, 3.0f);
-    auto v2 = vector_init(mem);
-    auto v3 = vector_init(8.0f, 9.0f, 10.0f, 11.0f);
-    auto v4 = vector_init(12.0f, 13.0f, 14.0f, 15.0f);
+    // auto v1 = vector_init(0.0f, 1.0f, 2.0f, 3.0f);
+    // auto v2 = vector_init(mem);
+    // auto v3 = vector_init(8.0f, 9.0f, 10.0f, 11.0f);
+    // auto v4 = vector_init(12.0f, 13.0f, 14.0f, 15.0f);
 
-    auto vp = v1 + v2;
-    auto vs = v1 - v2;
-    auto vm = v1 * v2;
-    auto vd = v1 / v2;
+    // auto vp = v1 + v2;
+    // auto vs = v1 - v2;
+    // auto vm = v1 * v2;
+    // auto vd = v1 / v2;
 
-    auto vms = v1 * 2.0;
-    auto vds = v1 / 2.0;
+    // auto vms = v1 * 2.0;
+    // auto vds = v1 / 2.0;
 
-    auto crossv = vector_cross_product(v1, v2);
+    // auto crossv = vector_cross_product(v1, v2);
 
-    auto len = vector_length(v1);
-    (void)len;
+    // auto len = vector_length(v1);
+    // (void)len;
 
-    vector_copy(vp, mem);
+    // vector_copy(vp, mem);
 
-    auto m1 = matrix_init(
-        0.01f, 0.02f, 0.03f, 0.04f,
-        0.05f, 0.06f, 0.07f, 0.08f,
-        0.09f, 0.10f, 0.11f, 0.12f,
-        0.13f, 0.14f, 0.15f, 0.16f
-    );
-    auto m2 = matrix_init(v1, v2, v3, v4);
-    auto mmul = matrix_mul(m1, m2);
-    float det = matrix_determinant(m2);
-    (void)det;
+    // auto m1 = matrix_init(
+    //     0.01f, 0.02f, 0.03f, 0.04f,
+    //     0.05f, 0.06f, 0.07f, 0.08f,
+    //     0.09f, 0.10f, 0.11f, 0.12f,
+    //     0.13f, 0.14f, 0.15f, 0.16f
+    // );
+    // auto m2 = matrix_init(v1, v2, v3, v4);
+    // auto mmul = matrix_mul(m1, m2);
+    // float det = matrix_determinant(m2);
+    // (void)det;
 
     return 0;
 }
