4e86e46b86fba1f3369d4d46127dd10fc3405334
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-08-13 23:36:07 +0700

    use enums utils

--- apps/math-demo/operations.hpp
+++ apps/math-demo/operations.hpp
@@ -6,25 +6,33 @@
 #pragma once
 
 namespace QCEMathDemo {
-    enum class MathOperation {
-        None,
-        VectorAddition,
-        VectorSubtraction,
-        VectorMultiplication,
-        VectorDivision,
-        VectorScalarMultiplication,
-        VectorScalarDivision,
-        VectorLength,
-        VectorNormalization,
-        VectorDotProduct,
-        VectorCrossProduct,
-        VectorMatrixMultiplication,
-        MatrixAddition,
-        MatrixSubtraction,
-        MatrixMultiplication,
-        MatrixTransposition,
-        MatrixDeterminant,
-        MatrixInvert,
-        MatrixVectorMultiplication
-    };
+#define CU_ENUMS_DESCRIPTION \
+    CU_BEGIN_ENUM(MathType) \
+        CU_VALUED_ENUM_UNIT(Scalar, 1) \
+        CU_VALUED_ENUM_UNIT(Vector, 4) \
+        CU_VALUED_ENUM_UNIT(Matrix, 16) \
+    CU_END_ENUM(MathType) \
+    CU_BEGIN_ENUM(MathOperation) \
+        CU_ENUM_UNIT(None) \
+        CU_ENUM_UNIT(VectorAddition) \
+        CU_ENUM_UNIT(VectorSubtraction) \
+        CU_ENUM_UNIT(VectorMultiplication) \
+        CU_ENUM_UNIT(VectorDivision) \
+        CU_ENUM_UNIT(VectorDotProduct) \
+        CU_ENUM_UNIT(VectorCrossProduct) \
+        CU_ENUM_UNIT(VectorScalarMultiplication) \
+        CU_ENUM_UNIT(VectorScalarDivision) \
+        CU_ENUM_UNIT(VectorLength) \
+        CU_ENUM_UNIT(VectorNormalization) \
+        CU_ENUM_UNIT(VectorMatrixMultiplication) \
+        CU_ENUM_UNIT(MatrixAddition) \
+        CU_ENUM_UNIT(MatrixSubtraction) \
+        CU_ENUM_UNIT(MatrixMultiplication) \
+        CU_ENUM_UNIT(MatrixTransposition) \
+        CU_ENUM_UNIT(MatrixDeterminant) \
+        CU_ENUM_UNIT(MatrixInvert) \
+        CU_ENUM_UNIT(MatrixVectorMultiplication) \
+    CU_END_ENUM(MathOperation)
+
+#include <cu/enum-utils.hpp>
 }
