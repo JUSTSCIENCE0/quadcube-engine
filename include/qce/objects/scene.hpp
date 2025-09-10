87aeeb59be77cd0d1c1b21377f9e5eaa37140e2c
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-09-10 22:47:05 +0700

    scene draft

---
+++ include/qce/objects/scene.hpp
@@ -0,0 +1,34 @@
+// Copyright (c) 2025, Yakov Usoltsev
+// Email: yakovmen62@gmail.com
+//
+// License: MIT
+
+#pragma once
+
+#include <qce/objects/entity.hpp>
+#include <qce/objects/resource_manager.hpp>
+
+#include <list>
+
+namespace QCE {
+    class Scene {
+        explicit Scene(ResourceManager& rm) :
+            m_resource_manager(rm) {}
+        Scene(const Scene&) = delete;
+        Scene(Scene&&) = delete;
+        Scene& operator=(const Scene&) = delete;
+        Scene& operator=(Scene&&) = delete;
+
+        ErrorCode AddEntity(
+                const std::string name,
+                const std::string model) {
+            // TODO
+            return ErrorCode::SUCCESS;
+        }
+
+    private:
+        ResourceManager& m_resource_manager;
+
+        std::list<std::shared_ptr<Entity>> m_entities{};
+    };
+}
