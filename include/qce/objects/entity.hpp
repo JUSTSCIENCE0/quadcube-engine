1848af62915c4b8b8feee5fc288762ee1d65e140
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-09-11 22:09:14 +0700

    add Entity constructor

--- include/qce/objects/entity.hpp
+++ include/qce/objects/entity.hpp
@@ -9,13 +9,17 @@
 
 namespace QCE {
     class Entity {
-
+        explicit Entity(std::string name, std::shared_ptr<Model> model) :
+            m_id(std::move(name)),
+            m_model(std::move(model)) {}
+        Entity(const Entity&) = delete;
+        Entity(Entity&&) = delete;
+        Entity& operator=(const Entity&) = delete;
+        Entity& operator=(Entity&&) = delete;
 
     private:
-        std::string id;
-
+        std::string m_id;
         std::shared_ptr<Model> m_model = nullptr;
-
         transform m_transform{};
     };
 }
