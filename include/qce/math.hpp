8ccaacc3c86301dc8edfe246e06df38cb5088da0
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-09-23 22:37:12 +0700

    coordinate -> coordinate3d

--- include/qce/math.hpp
+++ include/qce/math.hpp
@@ -9,14 +9,14 @@
 #include <qce/matrix.hpp>
 
 namespace QCE {
-    struct coordinate {
+    struct coordinate3d {
         float x = 0.0f;
         float y = 0.0f;
         float z = 0.0f;
     };
 
     struct vertex {
-        coordinate position{};
+        coordinate3d position{};
     };
 
     struct alignas(REQUIRED_ALIGNAS) transform {
@@ -24,7 +24,7 @@ namespace QCE {
         vector rotation = vector_zero(); // TODO: quaternion
 
         // not aligned
-        coordinate position{};
-        coordinate scale = { 1.0f, 1.0f, 1.0f };
+        coordinate3d position{};
+        coordinate3d scale = { 1.0f, 1.0f, 1.0f };
     };
 }
