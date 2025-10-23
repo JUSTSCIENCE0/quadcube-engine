573c6a5b58cafbbdb5ce67b929c3825f5b588304
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-10-23 20:21:57 +0700

    add dev experiments console

---
+++ apps/test-console/main.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2025, Yakov Usoltsev
+// Email: yakovmen62@gmail.com
+//
+// License: MIT
+
+#include <qce/math.hpp>
+#include <cu/file-utils.hpp>
+
+using namespace QCE;
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
