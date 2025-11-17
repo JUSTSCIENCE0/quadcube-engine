85a28023cf608154bebb95318dea22e68cbc3ad0
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-11-17 18:19:58 +0700

    events descr draft

---
+++ include/qce/hid/events.hpp
@@ -0,0 +1,97 @@
+// Copyright (c) 2025, Yakov Usoltsev
+// Email: yakovmen62@gmail.com
+//
+// License: MIT
+
+#pragma once
+
+namespace QCE {
+    /*
+    Events: 
+        Button Down
+        Button Up
+        Wheel Scroll
+        Mouse Move
+        Trigger Press
+        Stick Move
+
+    Codes:
+        Free: 7...12, 119...127
+
+        MOUSE_LB = 0,
+        MOUSE_RB = 1,
+        MOUSE_MB = 2,
+        MOUSE_B4 = 3, B - Button
+        MOUSE_B5 = 4,
+        MOUSE_SCROLL = 5,
+        MOUSE_MOVE = 6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        GAMEPAD_LOGO = 13,
+        GAMEPAD_START  = 14,
+        GAMEPAD_SELECT = 15,
+        GAMEPAD_LPAD_UP    = 16,
+        GAMEPAD_LPAD_RIGHT = 17,
+        GAMEPAD_LPAD_DOWN  = 18,
+        GAMEPAD_LPAD_LEFT  = 19,
+        GAMEPAD_RPAD_UP    = 20,
+        GAMEPAD_RPAD_RIGHT = 21,
+        GAMEPAD_RPAD_DOWN  = 22,
+        GAMEPAD_RPAD_LEFT  = 23,
+        GAMEPAD_LSHOULDER  = 24,
+        GAMEPAD_RSHOULDER  = 25,
+        GAMEPAD_LTRIGGER   = 26, // wo UP/DOWN
+        GAMEPAD_RTRIGGER   = 27, // wo UP/DOWN
+        GAMEPAD_LSTICK_BUTTON = 28,
+        GAMEPAD_RSTICK_BUTTON = 29,
+        GAMEPAD_LSTICK_MOVE   = 30, // wo UP/DOWN
+        GAMEPAD_RSTICK_MOVE   = 31, // wo UP/DOWN
+        KEYBOARD_SPACE = 32,
+        KEYBOARD_ENTER = 33,
+        KEYBOARD_TILDE = 34,
+        KEYBOARD_ESC = 35,
+        KEYBOARD_FUNCTIONAL = 36...47
+        KEYBOARD_NUMBERS    = 48...57,
+        KEYBOARD_UP         = 58,
+        KEYBOARD_RIGHT      = 59,
+        KEYBOARD_DOWN       = 60,
+        KEYBOARD_LEFT       = 61,
+        KEYBOARD_CTRL      = 62,
+        KEYBOARD_SHIFT     = 63,
+        KEYBOARD_CAPS_LOCK = 64,
+        KEYBOARD_CHARS = 65...90,
+        KEYBOARD_NUM_NUMBERS = 91...100,
+        KEYBOARD_TAB = 101,
+        KEYBOARD_INSERT = 102,
+        KEYBOARD_HOME = 103,
+        KEYBOARD_PG_UP = 104,
+        KEYBOARD_PG_DN = 105,
+        KEYBOARD_END = 106,
+        KEYBOARD_DELETE = 107,
+        KEYBOARD_BACKSPACE = 108,
+        KEYBOARD_COMMA = 109,
+        KEYBOARD_DOT = 110,
+        KEYBOARD_FORWARD_SLASH = 111, // /
+        KEYBOARD_BACK_SLASH = 112,    // \
+        KEYBOARD_SEMICOLON = 113,     // ;
+        KEYBOARD_APOSTROPHE = 114,    // '
+        KEYBOARD_OPEN_BRACKET = 115,  // [
+        KEYBOARD_CLOSE_BRACKET = 116,  // ]
+        KEYBOARD_MINUS = 117, // -_
+        KEYBOARD_PLUS = 118,  // =+
+    */
+
+    /*
+    #define CU_ENUMS_DESCRIPTION \
+        CU_BEGIN_ENUM(Name) \
+            CU_ENUM_UNIT(Name) \
+            CU_VALUED_ENUM_UNIT(Name, Value) \
+        CU_END_ENUM(Name)
+    #include <cu/enum-utils.hpp>
+    #undef CU_ENUMS_DESCRIPTION
+    */
+}
