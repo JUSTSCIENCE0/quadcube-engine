fec93c1a12863ccd0412b8840cdb631c4c26e24d
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-11-18 20:30:13 +0700

    add enum for event param & code

--- include/qce/hid/events.hpp
+++ include/qce/hid/events.hpp
@@ -14,84 +14,83 @@ namespace QCE {
         Mouse Move
         Trigger Press
         Stick Move
-
-    Codes:
-        Free: 7...12, 119...127
-
-        MOUSE_LB = 0,
-        MOUSE_RB = 1,
-        MOUSE_MB = 2,
-        MOUSE_B4 = 3, B - Button
-        MOUSE_B5 = 4,
-        MOUSE_SCROLL = 5,
-        MOUSE_MOVE = 6,
-        7,
-        8,
-        9,
-        10,
-        11,
-        12,
-        GAMEPAD_LOGO = 13,
-        GAMEPAD_START  = 14,
-        GAMEPAD_SELECT = 15,
-        GAMEPAD_LPAD_UP    = 16,
-        GAMEPAD_LPAD_RIGHT = 17,
-        GAMEPAD_LPAD_DOWN  = 18,
-        GAMEPAD_LPAD_LEFT  = 19,
-        GAMEPAD_RPAD_UP    = 20,
-        GAMEPAD_RPAD_RIGHT = 21,
-        GAMEPAD_RPAD_DOWN  = 22,
-        GAMEPAD_RPAD_LEFT  = 23,
-        GAMEPAD_LSHOULDER  = 24,
-        GAMEPAD_RSHOULDER  = 25,
-        GAMEPAD_LTRIGGER   = 26, // wo UP/DOWN
-        GAMEPAD_RTRIGGER   = 27, // wo UP/DOWN
-        GAMEPAD_LSTICK_BUTTON = 28,
-        GAMEPAD_RSTICK_BUTTON = 29,
-        GAMEPAD_LSTICK_MOVE   = 30, // wo UP/DOWN
-        GAMEPAD_RSTICK_MOVE   = 31, // wo UP/DOWN
-        KEYBOARD_SPACE = 32,
-        KEYBOARD_ENTER = 33,
-        KEYBOARD_TILDE = 34,
-        KEYBOARD_ESC = 35,
-        KEYBOARD_FUNCTIONAL = 36...47
-        KEYBOARD_NUMBERS    = 48...57,
-        KEYBOARD_UP         = 58,
-        KEYBOARD_RIGHT      = 59,
-        KEYBOARD_DOWN       = 60,
-        KEYBOARD_LEFT       = 61,
-        KEYBOARD_CTRL      = 62,
-        KEYBOARD_SHIFT     = 63,
-        KEYBOARD_CAPS_LOCK = 64,
-        KEYBOARD_CHARS = 65...90,
-        KEYBOARD_NUM_NUMBERS = 91...100,
-        KEYBOARD_TAB = 101,
-        KEYBOARD_INSERT = 102,
-        KEYBOARD_HOME = 103,
-        KEYBOARD_PG_UP = 104,
-        KEYBOARD_PG_DN = 105,
-        KEYBOARD_END = 106,
-        KEYBOARD_DELETE = 107,
-        KEYBOARD_BACKSPACE = 108,
-        KEYBOARD_COMMA = 109,
-        KEYBOARD_DOT = 110,
-        KEYBOARD_FORWARD_SLASH = 111, // /
-        KEYBOARD_BACK_SLASH = 112,    // \
-        KEYBOARD_SEMICOLON = 113,     // ;
-        KEYBOARD_APOSTROPHE = 114,    // '
-        KEYBOARD_OPEN_BRACKET = 115,  // [
-        KEYBOARD_CLOSE_BRACKET = 116,  // ]
-        KEYBOARD_MINUS = 117, // -_
-        KEYBOARD_PLUS = 118,  // =+
     */
 
-    /*
+    static constexpr auto FUNC_KEYS_COUNT = 12;
+
     #define CU_ENUMS_DESCRIPTION \
-        CU_BEGIN_ENUM(Name) \
-            CU_ENUM_UNIT(Name) \
-            CU_VALUED_ENUM_UNIT(Name, Value) \
-        CU_END_ENUM(Name)
+        CU_BEGIN_ENUM(HidEventParamType) \
+            CU_ENUM_UNIT(E_HEPT_NONE) \
+            CU_ENUM_UNIT(E_HEPT_COORDINATES) \
+            CU_ENUM_UNIT(E_HEPT_INTENSITY) \
+            CU_ENUM_UNIT(E_HEPT_DISPLACEMENT) \
+        CU_END_ENUM(HidEventParamType) \
+        CU_BEGIN_ENUM(HidEventCode) \
+            /* free: 7...12, 119...127 */ \
+                   CU_ENUM_UNIT(E_HEC_MOUSE_LB) \
+                   CU_ENUM_UNIT(E_HEC_MOUSE_RB) \
+                   CU_ENUM_UNIT(E_HEC_MOUSE_MB) \
+                   CU_ENUM_UNIT(E_HEC_MOUSE_B4) \
+                   CU_ENUM_UNIT(E_HEC_MOUSE_B5) \
+                   CU_ENUM_UNIT(E_HEC_MOUSE_SCROLL) /* wo UP/DOWN */ \
+                   CU_ENUM_UNIT(E_HEC_MOUSE_MOVE)   /* wo UP/DOWN */ \
+            CU_VALUED_ENUM_UNIT(E_HEC_GAMEPAD_LOGO, 13) \
+                   CU_ENUM_UNIT(E_HEC_GAMEPAD_START) \
+                   CU_ENUM_UNIT(E_HEC_GAMEPAD_SELECT) \
+                   CU_ENUM_UNIT(E_HEC_GAMEPAD_LPAD_UP) \
+                   CU_ENUM_UNIT(E_HEC_GAMEPAD_LPAD_RIGHT) \
+                   CU_ENUM_UNIT(E_HEC_GAMEPAD_LPAD_DOWN) \
+                   CU_ENUM_UNIT(E_HEC_GAMEPAD_LPAD_LEFT) \
+                   CU_ENUM_UNIT(E_HEC_GAMEPAD_RPAD_UP) \
+                   CU_ENUM_UNIT(E_HEC_GAMEPAD_RPAD_RIGHT) \
+                   CU_ENUM_UNIT(E_HEC_GAMEPAD_RPAD_DOWN) \
+                   CU_ENUM_UNIT(E_HEC_GAMEPAD_RPAD_LEFT) \
+                   CU_ENUM_UNIT(E_HEC_GAMEPAD_LSHOULDER) \
+                   CU_ENUM_UNIT(E_HEC_GAMEPAD_RSHOULDER) \
+                   CU_ENUM_UNIT(E_HEC_GAMEPAD_LTRIGGER) /* wo UP/DOWN */ \
+                   CU_ENUM_UNIT(E_HEC_GAMEPAD_RTRIGGER) /* wo UP/DOWN */ \
+                   CU_ENUM_UNIT(E_HEC_GAMEPAD_LSTICK_BUTTON) \
+                   CU_ENUM_UNIT(E_HEC_GAMEPAD_RSTICK_BUTTON) \
+                   CU_ENUM_UNIT(E_HEC_GAMEPAD_LSTICK_MOVE) /* wo UP/DOWN */ \
+                   CU_ENUM_UNIT(E_HEC_GAMEPAD_RSTICK_MOVE) /* wo UP/DOWN */ \
+            CU_VALUED_ENUM_UNIT(E_HEC_KEYBOARD_SPACE, ' ') \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_ENTER) \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_TILDE) \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_ESC) \
+            CU_VALUED_ENUM_UNIT(E_HEC_KEYBOARD_FUNC_BEGIN, 36) \
+            CU_VALUED_ENUM_UNIT(E_HEC_KEYBOARD_FUNC_END, E_HEC_KEYBOARD_FUNC_BEGIN - 1 + FUNC_KEYS_COUNT) \
+            CU_VALUED_ENUM_UNIT(E_HEC_KEYBOARD_NUMBER_BEGIN, '0') \
+            CU_VALUED_ENUM_UNIT(E_HEC_KEYBOARD_NUMBER_END,   '9') \
+            CU_VALUED_ENUM_UNIT(E_HEC_KEYBOARD_UP, 58) \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_RIGHT) \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_DOWN) \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_LEFT) \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_CTRL) \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_SHIFT) \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_CAPS_LOCK) \
+            CU_VALUED_ENUM_UNIT(E_HEC_KEYBOARD_CHARS_BEGIN, 'A') \
+            CU_VALUED_ENUM_UNIT(E_HEC_KEYBOARD_CHARS_END,   'Z') \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_NUMPAD_START) \
+            CU_VALUED_ENUM_UNIT(E_HEC_KEYBOARD_NUMPAD_END, E_HEC_KEYBOARD_NUMPAD_START + ('9' - '0')) \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_TAB) \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_INSERT) \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_HOME) \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_PG_UP) \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_PG_DN) \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_END) \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_DELETE) \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_BACKSPACE) \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_COMMA) \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_DOT) \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_FORWARD_SLASH) /* /? */ \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_BACK_SLASH)    /* \| */ \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_SEMICOLON)     /* ;: */ \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_APOSTROPHE)    /* '" */ \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_OPEN_BRACKET)  /* [{ */ \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_CLOSE_BRACKET) /* ]} */ \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_MINUS)         /* -_ */ \
+                   CU_ENUM_UNIT(E_HEC_KEYBOARD_PLUS)          /* =+ */ \
+        CU_END_ENUM(HidEventCode)
     #include <cu/enum-utils.hpp>
     #undef CU_ENUMS_DESCRIPTION
-    */
 }
