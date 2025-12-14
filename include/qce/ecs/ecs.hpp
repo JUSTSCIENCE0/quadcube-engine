ed6f5e925e1ca751f5d6fc0e1232fd625162939d
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-12-14 21:57:54 +0700

    add EntityManager (QueryEntities - wip)

--- include/qce/ecs/ecs.hpp
+++ include/qce/ecs/ecs.hpp
@@ -6,3 +6,73 @@
 #pragma once
 
 #include <qce/ecs/component.hpp>
+#include <qce/ancillary/error_codes.hpp>
+
+namespace QCE {
+    using entity_id_t = CU::id_t;
+
+    template <typename... Components>
+    class EntityManager {
+    public:
+        entity_id_t AddEntity() {
+            return m_id_pool.LockId();
+        }
+
+        ErrorCode RemoveEntity(entity_id_t entity_id) {
+            if (!m_id_pool.CheckId(entity_id)) {
+                return ErrorCode::E_ENG_ENTITY_NOT_FOUND;
+            }
+
+            m_id_pool.FreeId(entity_id);
+            ([&]() {
+                auto& storage = std::get<ComponentStorage<Components>>(m_components);
+                storage.RemoveEntity(entity_id);
+            }(), ...);
+            return ErrorCode::SUCCESS;
+        }
+
+        template <typename Component>
+        ErrorCode AddComponent(entity_id_t entity_id, Component component) {
+            if (!m_id_pool.CheckId(entity_id)) {
+                return ErrorCode::E_ENG_ENTITY_NOT_FOUND;
+            }
+            auto& storage = std::get<ComponentStorage<Component>>(m_components);
+            storage.SetEntity(entity_id, std::move(component));
+            return ErrorCode::SUCCESS;
+        }
+
+        template <typename... Request>
+        std::set<entity_id_t> QueryEntities() const {
+            std::set<entity_id_t> result;
+            if constexpr (sizeof...(Request) == 0) {
+                return result;
+            }
+            else if constexpr (sizeof...(Request) == 1) {
+                using first_type_t = std::tuple_element_t<0, std::tuple<Request...>>;
+                const auto& storage = std::get<ComponentStorage<first_type_t>>(m_components);
+                return storage.GetActualEntities();
+            }
+            else {
+                std::vector<size_t> entity_counts{
+                    std::get<ComponentStorage<Request>>(m_components).Size()...
+                };
+
+                auto it = std::min_element(entity_counts.begin(), entity_counts.end());
+                if (0 == *it) {
+                    return result;
+                }
+
+                auto min_index = std::distance(entity_counts.begin(), it);
+
+                // TODO: get result from the minimal storage and check other storages for each entity
+                (void)min_index;
+
+                return result;
+            }
+        }
+
+    private:
+        CU::IdPool m_id_pool;
+        std::tuple<ComponentStorage<Components>...> m_components;
+    };
+}
---
+++ tests/ecs-test/entity-test.cpp
@@ -0,0 +1,68 @@
+// Copyright (c) 2025, Yakov Usoltsev
+// Email: yakovmen62@gmail.com
+//
+// License: MIT
+
+#include <gtest/gtest.h>
+
+#include <qce/ecs/ecs.hpp>
+
+TEST(EntityManagerTest, BaseFunctional) {
+    using Component1 = uint32_t;
+    using Component2 = float;
+    using Component3 = int64_t;
+
+    QCE::EntityManager<Component1, Component2, Component3> entity_manager;
+
+    auto entity1 = entity_manager.AddEntity();
+    auto entity2 = entity_manager.AddEntity();
+    auto entity3 = entity_manager.AddEntity();
+
+    ASSERT_EQ(entity1, 0);
+    ASSERT_EQ(entity2, 1);
+    ASSERT_EQ(entity3, 2);
+
+    // check invalid entity
+    ASSERT_EQ(QCE::ErrorCode::E_ENG_ENTITY_NOT_FOUND,
+              entity_manager.AddComponent<Component1>(100, 100));
+
+    /*         | entity1 | entity2 | entity3
+    -----------|---------|---------|---------
+    Component1 |   42    |  None   |  None
+    Component2 |  36.6   |  None   |  3.14
+    Component3 |  None   |   50    |   99  */
+
+    ASSERT_EQ(QCE::ErrorCode::SUCCESS, entity_manager.AddComponent<Component1>(entity1, 42));
+    ASSERT_EQ(QCE::ErrorCode::SUCCESS, entity_manager.AddComponent<Component2>(entity1, 36.6f));
+
+    ASSERT_EQ(QCE::ErrorCode::SUCCESS, entity_manager.AddComponent<Component3>(entity2, 50));
+
+    ASSERT_EQ(QCE::ErrorCode::SUCCESS, entity_manager.AddComponent<Component2>(entity3, 3.14f));
+    ASSERT_EQ(QCE::ErrorCode::SUCCESS, entity_manager.AddComponent<Component3>(entity3, 99));
+
+    // check queries
+    EXPECT_EQ(entity_manager.QueryEntities<Component1>(),
+              std::set<QCE::entity_id_t>({ entity1 }));
+    EXPECT_EQ(entity_manager.QueryEntities<Component2>(),
+              std::set<QCE::entity_id_t>({ entity1, entity3 }));
+    EXPECT_EQ(entity_manager.QueryEntities<Component3>(),
+              std::set<QCE::entity_id_t>({ entity2, entity3 }));
+    // TODO: check several components query
+
+    // remove entity
+    ASSERT_EQ(QCE::ErrorCode::E_ENG_ENTITY_NOT_FOUND, entity_manager.RemoveEntity(100));
+    ASSERT_EQ(QCE::ErrorCode::SUCCESS, entity_manager.RemoveEntity(entity2));
+
+    // check removed entity
+    ASSERT_EQ(QCE::ErrorCode::E_ENG_ENTITY_NOT_FOUND,
+              entity_manager.AddComponent<Component1>(entity2, 100));
+
+    // check queries
+    EXPECT_EQ(entity_manager.QueryEntities<Component1>(),
+              std::set<QCE::entity_id_t>({ entity1 }));
+    EXPECT_EQ(entity_manager.QueryEntities<Component2>(),
+              std::set<QCE::entity_id_t>({ entity1, entity3 }));
+    EXPECT_EQ(entity_manager.QueryEntities<Component3>(),
+              std::set<QCE::entity_id_t>({ entity3 }));
+    // TODO: check several components query
+}
