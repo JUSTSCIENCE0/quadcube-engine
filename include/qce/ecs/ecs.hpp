6627beb5b93ec56cff859d29b28f3ae28f63ea24
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-12-18 15:47:55 +0700

    entity manager query cache draft

--- include/qce/ecs/ecs.hpp
+++ include/qce/ecs/ecs.hpp
@@ -8,10 +8,106 @@
 #include <qce/ecs/component.hpp>
 #include <qce/ancillary/error_codes.hpp>
 
+#include <cassert>
 #include <algorithm>
+#include <bitset>
+#include <type_traits>
+#include <unordered_map>
+#include <list>
+#include <optional>
+
+#ifndef FRIEND_TEST
+#define  FRIEND_TEST(test_case_name, test_name)
+#endif
 
 namespace QCE {
     using entity_id_t = CU::id_t;
+
+    namespace PrivateImplementation {
+        template<typename T, typename... Ts>
+        constexpr bool contains_v = (std::is_same_v<T, Ts> || ...);
+    }
+
+    static constexpr size_t DISABLE_ENTITY_QUERY_CACHE = 0;
+    static constexpr auto   INFINITE_ENTITY_QUERY_CACHE = std::numeric_limits<size_t>::max();
+
+    template <size_t CacheLimit, typename... Components>
+    class EntityQueriesCache {
+    public:
+        template <typename... Query>
+        std::optional<std::set<entity_id_t>> Get() const noexcept {
+            if constexpr (CacheLimit == DISABLE_ENTITY_QUERY_CACHE) {
+                return std::nullopt;
+            }
+
+            auto mask = GetQueryMask<Query...>();
+            auto it = m_results.find(mask);
+            if (it == m_results.end()) {
+                return std::nullopt;
+            }
+
+            if constexpr (CacheLimit != INFINITE_ENTITY_QUERY_CACHE) {
+                auto usage_it = std::find(m_usage_order.begin(), m_usage_order.end(), mask);
+                assert(m_usage_order.end() != usage_it);
+                m_usage_order.splice(m_usage_order.begin(), m_usage_order, usage_it);
+            }
+
+            return it->second;
+        }
+
+        template <typename... Query>
+        void Put(std::set<entity_id_t> result) noexcept {
+            if constexpr (CacheLimit == DISABLE_ENTITY_QUERY_CACHE) {
+                return;
+            }
+            auto mask = GetQueryMask<Query...>();
+
+            assert(m_results.find(mask) == m_results.end());
+            assert(m_usage_order.end() == std::find(
+                m_usage_order.begin(), m_usage_order.end(), mask));
+
+            if constexpr (CacheLimit != INFINITE_ENTITY_QUERY_CACHE) {
+                assert(m_results.size() <= CacheLimit);
+
+                if (m_results.size() == CacheLimit) {
+                    auto lru_mask = m_usage_order.back();
+                    m_usage_order.pop_back();
+                    m_results.erase(lru_mask);
+                }
+                m_usage_order.push_front(mask);
+            }
+
+            m_results.insert(mask, std::move(result));
+        }
+
+        // TODO: Invalidate cache entries on component addition/removal
+
+
+    private:
+        FRIEND_TEST(EntityQueriesCacheTest, QueryMask);
+
+        static constexpr size_t ComponentsCount = sizeof...(Components);
+
+        using QueryMask = std::bitset<ComponentsCount>;
+
+        template <typename... Query>
+        static QueryMask GetQueryMask() noexcept {
+            constexpr bool flags[ComponentsCount] = {
+                PrivateImplementation::contains_v<Components, Query...>...
+            };
+
+            QueryMask mask{};
+            for (size_t i = 0; i < ComponentsCount; ++i) {
+                if (flags[i]) {
+                    mask.set(i);
+                }
+            }
+            return mask;
+        }
+
+        std::unordered_map<QueryMask, std::set<entity_id_t>> m_results{};
+        std::list<QueryMask> m_usage_order{};
+    };
 
     template <typename... Components>
     class EntityManager {
@@ -43,20 +139,20 @@ namespace QCE {
             return ErrorCode::SUCCESS;
         }
 
-        template <typename... Request>
+        template <typename... Query>
         std::set<entity_id_t> QueryEntities() const {
             std::set<entity_id_t> result;
-            if constexpr (sizeof...(Request) == 0) {
+            if constexpr (sizeof...(Query) == 0) {
                 return result;
             }
-            else if constexpr (sizeof...(Request) == 1) {
-                using first_type_t = std::tuple_element_t<0, std::tuple<Request...>>;
+            else if constexpr (sizeof...(Query) == 1) {
+                using first_type_t = std::tuple_element_t<0, std::tuple<Query...>>;
                 const auto& storage = std::get<ComponentStorage<first_type_t>>(m_components);
                 return storage.GetActualEntities();
             }
             else {
                 std::vector<size_t> entity_counts{
-                    std::get<ComponentStorage<Request>>(m_components).Size()...
+                    std::get<ComponentStorage<Query>>(m_components).Size()...
                 };
 
                 auto it = std::min_element(entity_counts.begin(), entity_counts.end());
@@ -69,7 +165,7 @@ namespace QCE {
 
                 ([&]() {
                     if (index == min_index) {
-                        const auto& storage = std::get<ComponentStorage<Request>>(m_components);
+                        const auto& storage = std::get<ComponentStorage<Query>>(m_components);
                         result = storage.GetActualEntities();
                     }
                     index++;
@@ -77,7 +173,7 @@ namespace QCE {
 
                 ([&]() {
                     if (index != min_index) {
-                        const auto& storage = std::get<ComponentStorage<Request>>(m_components);
+                        const auto& storage = std::get<ComponentStorage<Query>>(m_components);
                         std::erase_if(result, 
                             [&storage](entity_id_t id) { return !storage.HasEntity(id); });
                     }
--- tests/ecs-test/entity-test.cpp
+++ tests/ecs-test/entity-test.cpp
@@ -7,8 +7,47 @@
 
 #include <qce/ecs/ecs.hpp>
 
+namespace QCE {
+    struct Component1 { int val = 0; };
+    struct Component2 { int val = 0; };
+    struct Component3 { int val = 0; };
+    struct Component4 { int val = 0; };
+    struct Component5 { int val = 0; };
+
+    class EntityQueriesCacheTest :
+            public testing::Test {
+    protected:
+        EntityQueriesCache<
+            64,
+            Component1,
+            Component2,
+            Component3,
+            Component4,
+            Component5> m_test_cache{};
+    };
+
+    TEST_F(EntityQueriesCacheTest, QueryMask) {
+        auto mask = m_test_cache.GetQueryMask<Component1, Component2, Component5>();
+        std::bitset<5> expected_mask(0b10011ull);
+        EXPECT_EQ(mask, expected_mask);
+
+        mask = m_test_cache.GetQueryMask<Component4, Component1>();
+        expected_mask = std::bitset<5>(0b01001ull);
+        EXPECT_EQ(mask, expected_mask);
+
+        mask = m_test_cache.GetQueryMask<Component1, Component3, Component5, Component2, Component4>();
+        expected_mask = std::bitset<5>(0b11111ull);
+        EXPECT_EQ(mask, expected_mask);
+
+        mask = m_test_cache.GetQueryMask<>();
+        expected_mask = std::bitset<5>(0b00000ull);
+        EXPECT_EQ(mask, expected_mask);
+    }
+
+    // TODO: check query cache working
+}
+
 // TODO: refactore tests - use fixtures
-// TODO: check query cache working
 
 TEST(EntityManagerTest, BaseFunctional) {
     using Component1 = uint32_t;
