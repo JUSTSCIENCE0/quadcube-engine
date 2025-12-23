efc318ecc4e03e6b07a4c51cc894493dbfac4eb8
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-12-23 17:03:43 +0700

    add ECS benchmark for dynamic scene

--- apps/ecs-benchmark/main.cpp
+++ apps/ecs-benchmark/main.cpp
@@ -19,6 +19,9 @@ using namespace QCE;
 bool random_bool() {
     return (std::rand() % 2) == 0;
 }
+entity_id_t random_id(entity_id_t max_id) {
+    return static_cast<entity_id_t>(std::rand()) % max_id;
+}
 
 // components simulation
 template<std::size_t N> struct C {};
@@ -58,8 +61,27 @@ std::set<entity_id_t> run_query(
 static constexpr size_t ENTITIES_COUNT = 32768;
 static constexpr size_t QUERY_REPEATS = 1000;
 
-template <size_t CacheLimit>
-void query_performance_static() {
+template <size_t InvalidatePeriod, size_t CacheLimit, size_t QuerySize>
+std::string GenerateTestID() {
+    std::string test_id = "";
+    if constexpr (!InvalidatePeriod)
+        test_id += "Static ";
+    else
+        test_id += "Dynamic_" + std::to_string(InvalidatePeriod) + " ";
+
+    if constexpr (CacheLimit == DISABLE_ENTITY_QUERY_CACHE)
+        test_id += "NoCache ";
+    else if constexpr (CacheLimit == INFINITE_ENTITY_QUERY_CACHE)
+        test_id += "InfiniteCache ";
+    else
+        test_id += "CacheLimit_" + std::to_string(CacheLimit) + " ";
+
+    test_id += "Components_" + std::to_string(QuerySize);
+    return test_id;
+}
+
+template <size_t CacheLimit, size_t InvalidatePeriod>
+void query_performance_benchmark() {
     std::srand(777);
 
     std::set<int> empty_results{};
@@ -71,47 +93,48 @@ void query_performance_static() {
         add_random_components<CacheLimit>(ecm, entity_id, std::make_index_sequence<COMPONENTS_COUNT>{});
     }
 
-    std::string test_id;
-    if constexpr (CacheLimit == DISABLE_ENTITY_QUERY_CACHE)
-        test_id = "NoCache";
-    else if constexpr (CacheLimit == INFINITE_ENTITY_QUERY_CACHE)
-        test_id = "InfiniteCache";
-    else
-        test_id = "CacheLimit_" + std::to_string(CacheLimit);
+    auto test_id_2 =  GenerateTestID<InvalidatePeriod, CacheLimit, 2>();
+    auto test_id_4 =  GenerateTestID<InvalidatePeriod, CacheLimit, 4>();
+    auto test_id_8 =  GenerateTestID<InvalidatePeriod, CacheLimit, 8>();
+    auto test_id_64 = GenerateTestID<InvalidatePeriod, CacheLimit, 64>();
 
     for (size_t i = 0; i < QUERY_REPEATS; ++i) {
-        CU_PROFILE_CHECKBLOCK(Components_2, test_id + " Components_2");
-
-        auto res = run_query<CacheLimit>(ecm, std::make_index_sequence<2>{});
-        if (res.size() == 0) {
-            empty_results.insert(2);
+        {
+            CU_PROFILE_CHECKBLOCK(Components_2, test_id_2);
+            auto res = run_query<CacheLimit>(ecm, std::make_index_sequence<2>{});
+            if (res.size() == 0) {
+                empty_results.insert(2);
+            }
         }
-    }
-
-    for (size_t i = 0; i < QUERY_REPEATS; ++i) {
-        CU_PROFILE_CHECKBLOCK(Components_4, test_id + " Components_4");
-
-        auto res = run_query<CacheLimit>(ecm, std::make_index_sequence<4>{});
-        if (res.size() == 0) {
-            empty_results.insert(4);
+        {
+            CU_PROFILE_CHECKBLOCK(Components_4, test_id_4);
+            auto res = run_query<CacheLimit>(ecm, std::make_index_sequence<4>{});
+            if (res.size() == 0) {
+                empty_results.insert(4);
+            }
         }
-    }
-
-    for (size_t i = 0; i < QUERY_REPEATS; ++i) {
-        CU_PROFILE_CHECKBLOCK(Components_8, test_id + " Components_8");
-
-        auto res = run_query<CacheLimit>(ecm, std::make_index_sequence<8>{});
-        if (res.size() == 0) {
-            empty_results.insert(8);
+        {
+            CU_PROFILE_CHECKBLOCK(Components_8, test_id_8);
+            auto res = run_query<CacheLimit>(ecm, std::make_index_sequence<8>{});
+            if (res.size() == 0) {
+                empty_results.insert(8);
+            }
         }
-    }
+        {
+            CU_PROFILE_CHECKBLOCK(Components_64, test_id_64);
+            auto res = run_query<CacheLimit>(ecm, std::make_index_sequence<64>{});
+            if (res.size() == 0) {
+                empty_results.insert(64);
+            }
+        }
 
-    for (size_t i = 0; i < QUERY_REPEATS; ++i) {
-        CU_PROFILE_CHECKBLOCK(Components_64, test_id + " Components_64");
 
-        auto res = run_query<CacheLimit>(ecm, std::make_index_sequence<64>{});
-        if (res.size() == 0) {
-            empty_results.insert(64);
+        if (InvalidatePeriod && i % InvalidatePeriod == 0) {
+            // invalidate cache
+            auto entity_id = random_id(ENTITIES_COUNT);
+            QCE_THROW_CRITICAL(ecm.RemoveEntity(entity_id));
+            entity_id = ecm.AddEntity();
+            add_random_components<CacheLimit>(ecm, entity_id, std::make_index_sequence<COMPONENTS_COUNT>{});
         }
     }
 
@@ -122,12 +145,8 @@ void query_performance_static() {
     std::cout << std::endl;
 
     auto bench_res = CU_PROFILE_GET_RESULTS(
-        std::vector{ 
-            test_id + " Components_2",
-            test_id + " Components_4",
-            test_id + " Components_8",
-            test_id + " Components_64"
-        });
+        std::vector{ test_id_2, test_id_4, test_id_8, test_id_64 }
+    );
 
     for (const auto& [res_id, timer_result] : bench_res) {
         std::cout << res_id << ":" << std::endl;
@@ -139,11 +158,17 @@ void query_performance_static() {
 }
 
 int main(int argc, char* argv[]) {
-    query_performance_static<DISABLE_ENTITY_QUERY_CACHE>();
+    query_performance_benchmark<DISABLE_ENTITY_QUERY_CACHE, 0>();
+    query_performance_benchmark<64, 0>();
+    query_performance_benchmark<INFINITE_ENTITY_QUERY_CACHE, 0>();
 
-    query_performance_static<64>();
+    query_performance_benchmark<DISABLE_ENTITY_QUERY_CACHE, 30>();
+    query_performance_benchmark<64, 30>();
+    query_performance_benchmark<INFINITE_ENTITY_QUERY_CACHE, 30>();
 
-    query_performance_static<INFINITE_ENTITY_QUERY_CACHE>();
+    query_performance_benchmark<DISABLE_ENTITY_QUERY_CACHE, 1>();
+    query_performance_benchmark<64, 1>();
+    query_performance_benchmark<INFINITE_ENTITY_QUERY_CACHE, 1>();
 
     return 0;
 }
