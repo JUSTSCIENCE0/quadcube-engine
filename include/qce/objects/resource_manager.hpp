cbec38f5ec78f3464ab3290bacde5e263aead4c5
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-12-28 15:02:11 +0700

    add resourse storage

--- include/qce/objects/resource_manager.hpp
+++ include/qce/objects/resource_manager.hpp
@@ -16,10 +16,23 @@
 #include <concepts>
 #include <type_traits>
 #include <memory>
+#include <vector>
+#include <set>
+#include <limits>
 
 namespace QCE {
+    template<typename T>
+    concept Resource = requires(T t) {
+        requires std::same_as<
+            std::remove_cvref_t<decltype(t.id)>,
+            std::string
+        >;
+    };
+
     class ResourceManager final {
     public:
+        static constexpr auto INVALID_RESOURCE_INDEX = std::numeric_limits<size_t>::max();
+
         ResourceManager(const ResourceManager&) = delete;
         ResourceManager(ResourceManager&&) = delete;
         ResourceManager& operator=(const ResourceManager&) = delete;
@@ -95,6 +108,82 @@ namespace QCE {
         /// types
         template <typename T>
         using Storage = std::unordered_map<std::string, std::shared_ptr<T>>;
+
+        template <Resource ResourceT>
+        class ResourceStorage {
+        public:
+            ErrorCode Add(ResourceT resource) {
+                std::string id = resource.id;
+
+                if (m_index.end() != m_index.find(id))
+                    return ErrorCode::E_RM_RESOURCE_ALREADY_EXISTS;
+
+                if (!m_free_indices.empty()) {
+                    auto index = *m_free_indices.begin();
+                    m_data[index] = std::move(resource);
+                    m_index.emplace(std::move(id), index);
+                    m_free_indices.erase(index);
+                }
+                else {
+                    m_data.push_back(std::move(resource));
+                    m_index.emplace(std::move(id), m_data.size() - 1);
+                }
+
+                return ErrorCode::SUCCESS;
+            }
+
+            bool Exists(size_t index) {
+                if (m_data.size() <= index)
+                    return false;
+
+                return (m_free_indices.end() != m_free_indices.find(index));
+            }
+
+            bool Exists(const std::string& id) {
+                return (m_index.end() != m_index.find(id));
+            }
+
+            size_t GetIndex(const std::string& id) {
+                auto result = m_index.find(id);
+                if (m_index.end() == result)
+                    return INVALID_RESOURCE_INDEX;
+                return result->second;
+            }
+
+            ResourceT& Get(size_t index) {
+                assert(Exists(index));
+                return m_data[index];
+            }
+
+            ErrorCode Remove(size_t index) {
+                if (!Exists(index))
+                    return ErrorCode::E_RM_RESOURCE_NOT_FOUND;
+
+                std::string id = m_data[index].id;
+                m_index.erase(id);
+
+                m_free_indices.insert(index);
+
+                if constexpr (requires(ResourceT rs) { rs.reset(); }) {
+                    m_data[index].reset();
+                }
+
+                return ErrorCode::SUCCESS;
+            }
+
+            ErrorCode Remove(const std::string& id) {
+                size_t index = GetIndex(id);
+                if (INVALID_RESOURCE_INDEX == index)
+                    return ErrorCode::E_RM_RESOURCE_NOT_FOUND;
+
+                Remove(index);
+            }
+
+        private:
+            std::vector<ResourceT> m_data{};
+            std::unordered_map<std::string, size_t> m_index{};
+            std::set<size_t> m_free_indices{};
+        };
 
         /// consts
         static constexpr auto RESOURCES_DIRECTORY = "resources";
