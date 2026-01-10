5fd4dffecfa803baa513182903d20c6cf3337213
JUSTSCIENCE0 <yakovmen62@gmail.com>
2026-01-10 13:58:52 +0700

    update copyright

--- src/about.cpp
+++ src/about.cpp
@@ -17,7 +17,7 @@ namespace QCE {
 
     const char* get_licence() {
         return  "MIT License\n\n"
-            "Copyright(c) 2025, Yakov Usoltsev\n\n"
+            "Copyright(c) 2025-2026, Yakov Usoltsev\n\n"
             "Permission is hereby granted, free of charge, to any person obtaining a copy "
             "of this software and associated documentation files(the \"Software\"), to deal "
             "in the Software without restriction, including without limitation the rights "
