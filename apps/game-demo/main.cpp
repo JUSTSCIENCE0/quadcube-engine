f7cec175532c8dffd41e3915f33678fb73f5ae46
JUSTSCIENCE0 <yakovmen62@gmail.com>
2026-04-12 18:46:42 +0700

    use json config files

--- apps/game-demo/main.cpp
+++ apps/game-demo/main.cpp
@@ -13,6 +13,8 @@ int main(int argc, char* argv[]) {
     auto& app = QCE::Application<>::Get();
 
     const auto CONFIGS_DIR = app.m_systems.GetConfigsDirectory();
+    std::string error_descr = "";
+    auto parse_result = macrojson::E_MJSON_OK;
 
     QCE::ApplicationConfig<> config{
         .graphics_output {
@@ -20,22 +22,30 @@ int main(int argc, char* argv[]) {
             .is_first_person = true
         }
     };
+
+    const auto camera_config_json_file = CONFIGS_DIR / "camera_system.json";
     auto& camera_config = std::get<QCE::CameraConfig>(config.systems_configs);
-    camera_config = QCE::DEFAULT_CAMERA_CONFIG;
+    parse_result = macrojson::json_file_to_object(camera_config_json_file, camera_config, error_descr);
+    if (macrojson::E_MJSON_OK != parse_result) {
+        std::cerr << "Camera Config parsing failed with error: " << error_descr << std::endl;
+        return -1;
+    }
 
+    const auto hid_events_config_json_file = CONFIGS_DIR / "hid_system.json";
     auto& hid_events_config = std::get<QCE::HidConfig>(config.systems_configs);
-    auto hid_events_config_json_file = CONFIGS_DIR;
-    hid_events_config_json_file.append("hid_system.json");
-
-    std::string error_descr = "";
-    auto parse_result = macrojson::json_file_to_object(hid_events_config_json_file, hid_events_config, error_descr);
+    parse_result = macrojson::json_file_to_object(hid_events_config_json_file, hid_events_config, error_descr);
     if (macrojson::E_MJSON_OK != parse_result) {
         std::cerr << "HID Config parsing failed with error: " << error_descr << std::endl;
         return -1;
     }
 
+    const auto render_config_json_file = CONFIGS_DIR / "render_system.json";
     auto& render_config = std::get<QCE::RenderConfig>(config.systems_configs);
-    render_config = QCE::DEFAULT_RENDER_CONFIG;
+    parse_result = macrojson::json_file_to_object(render_config_json_file, render_config, error_descr);
+    if (macrojson::E_MJSON_OK != parse_result) {
+        std::cerr << "Render Config parsing failed with error: " << error_descr << std::endl;
+        return -1;
+    }
 
     QCE_CRITICAL(app.Setup(config));
 
