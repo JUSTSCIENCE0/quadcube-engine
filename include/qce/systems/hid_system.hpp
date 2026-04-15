55fb7a202fcdfe3d2d0cbc7f1985084c799f6939
JUSTSCIENCE0 <yakovmen62@gmail.com>
2026-04-15 22:06:19 +0700

    move hid system methods implementation from header

--- include/qce/systems/hid_system.hpp
+++ include/qce/systems/hid_system.hpp
@@ -17,82 +17,18 @@ namespace QCE {
 
         explicit HidSystem(Entities&) {}
 
-        ErrorCode Setup(const Config& config) {
-            // register default handler
-            QCE_CRITICAL(
-                ResourceManager::Get().Add(Command{
-                    "HidDescribe",
-                    std::make_shared<HidDescribe>()
-                })
-            );
+        ErrorCode Setup(const Config& config);
 
-            for (const auto& descr : config.event_descriptors) {
-                assert(descr);
-                switch (descr->type) {
-                case HidEventType::E_HET_SINGLE: {
-                    auto single_descr = static_cast<const HidSingleEvent*>(descr.get());
-                    auto hid_event_code = hid_event_from_short_name(single_descr->hid_event_code_name.c_str());
-                    m_single_event_handlers[hid_event_code] =
-                        ResourceManager::Get().Read<Command>(single_descr->handler).command;
-                    if (!m_single_event_handlers[hid_event_code])
-                        return ErrorCode::E_RM_COMMAND_NOT_FOUND;
-                    break;
-                }
-                case HidEventType::E_HET_ACCORD: {
-                    // TODO
-                    break;
-                }
-                case HidEventType::E_HET_COMBO: {
-                    // TODO
-                    break;
-                }
-                default: {
-                    assert(!"Invalid HidEventType");
-                    // TODO - use log system
-                    std::cout << "Ignore invalid HidEventType " << descr->type << std::endl;
-                    break;
-                }
-                }
-            }
+        void PushEvent(const HidEvent& hid_event);
+        void PushMouseMoveEvent(HidEvent hid_event);
 
-            return ErrorCode::SUCCESS;
-        }
-
-        void PushEvent(const HidEvent& hid_event) {
-            m_events_queue.push_back(hid_event);
-        }
-        void PushMouseMoveEvent(HidEvent hid_event) {
-            assert(HidEventCode::E_HEC_MOUSE_MOVE == hid_event.GetCode());
-            m_move_event.emplace(std::move(hid_event));
-        }
-
-        ErrorCode Update() {
-            // TODO - parse & handle events sequence
-            ProcessSingleEvents();
-
-            return ErrorCode::SUCCESS;
-        }
+        ErrorCode Update();
 
     private:
         using HidEventsQueue = std::deque<HidEvent>;
         using SingleEventHandlersMap = std::array<std::shared_ptr<BaseCommand>, HidEventCode::E_HEC_COUNT>;
 
-        void ProcessSingleEvents() {
-            if (m_single_event_handlers[HidEventCode::E_HEC_MOUSE_MOVE] &&
-                m_move_event.has_value()) {
-                auto& handler = m_single_event_handlers[HidEventCode::E_HEC_MOUSE_MOVE];
-                if (handler)
-                    QCE_SOFT(handler->Execute(&m_move_event.value()));
-                m_move_event.reset();
-            }
-
-            for (const auto& event : m_events_queue) {
-                auto& handler = m_single_event_handlers[event.GetCode()];
-                if (handler)
-                    QCE_SOFT(handler->Execute(&event));
-            }
-            m_events_queue.clear();
-        }
+        void ProcessSingleEvents();
 
         std::optional<HidEvent> m_move_event;
         HidEventsQueue m_events_queue{};
---
+++ src/hid_system.cpp
@@ -0,0 +1,82 @@
+// Copyright (c) 2026, Yakov Usoltsev
+// Email: yakovmen62@gmail.com
+//
+// License: MIT
+
+#include <qce/systems/hid_system.hpp>
+
+namespace QCE {
+    ErrorCode HidSystem::Setup(const Config& config) {
+        // register default handler
+        QCE_CRITICAL(
+            ResourceManager::Get().Add(Command{
+                "HidDescribe",
+                std::make_shared<HidDescribe>()
+            })
+        );
+
+        for (const auto& descr : config.event_descriptors) {
+            assert(descr);
+            switch (descr->type) {
+            case HidEventType::E_HET_SINGLE: {
+                auto single_descr = static_cast<const HidSingleEvent*>(descr.get());
+                auto hid_event_code = hid_event_from_short_name(single_descr->hid_event_code_name.c_str());
+                m_single_event_handlers[hid_event_code] =
+                    ResourceManager::Get().Read<Command>(single_descr->handler).command;
+                if (!m_single_event_handlers[hid_event_code])
+                    return ErrorCode::E_RM_COMMAND_NOT_FOUND;
+                break;
+            }
+            case HidEventType::E_HET_ACCORD: {
+                // TODO
+                break;
+            }
+            case HidEventType::E_HET_COMBO: {
+                // TODO
+                break;
+            }
+            default: {
+                assert(!"Invalid HidEventType");
+                // TODO - use log system
+                std::cout << "Ignore invalid HidEventType " << descr->type << std::endl;
+                break;
+            }
+            }
+        }
+
+        return ErrorCode::SUCCESS;
+    }
+
+    void HidSystem::PushEvent(const HidEvent& hid_event) {
+        m_events_queue.push_back(hid_event);
+    }
+
+    void HidSystem::PushMouseMoveEvent(HidEvent hid_event) {
+        assert(HidEventCode::E_HEC_MOUSE_MOVE == hid_event.GetCode());
+        m_move_event.emplace(std::move(hid_event));
+    }
+
+    ErrorCode HidSystem::Update() {
+        // TODO - parse & handle events sequence
+        ProcessSingleEvents();
+
+        return ErrorCode::SUCCESS;
+    }
+
+    void HidSystem::ProcessSingleEvents() {
+        if (m_single_event_handlers[HidEventCode::E_HEC_MOUSE_MOVE] &&
+            m_move_event.has_value()) {
+            auto& handler = m_single_event_handlers[HidEventCode::E_HEC_MOUSE_MOVE];
+            if (handler)
+                QCE_SOFT(handler->Execute(&m_move_event.value()));
+            m_move_event.reset();
+        }
+
+        for (const auto& event : m_events_queue) {
+            auto& handler = m_single_event_handlers[event.GetCode()];
+            if (handler)
+                QCE_SOFT(handler->Execute(&event));
+        }
+        m_events_queue.clear();
+    }
+}
