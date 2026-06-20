c0366edaf3dc617daf37dc1819417fc16dc7176b
JUSTSCIENCE0 <yakovmen62@gmail.com>
2026-06-20 21:21:39 +0700

    add sphere to demo

--- apps/game-demo/main.cpp
+++ apps/game-demo/main.cpp
@@ -20,6 +20,11 @@ int main(int argc, char* argv[]) {
         .height = 1.0f
     };
     QCE_CRITICAL(app.Resources().AddFigure(cuboid, "cuboid"));
+    QCE::SphereParams sphere{
+        .radius = 1.0f,
+        .subdivisions = 3
+    };
+    QCE_CRITICAL(app.Resources().AddFigure(sphere, "sphere"));
     QCE_CRITICAL(app.Resources().AddTexture("squares.bc7"));
     QCE::Material textured_material{};
     textured_material.id = "textured_material";
@@ -33,6 +38,9 @@ int main(int argc, char* argv[]) {
 
     QCE::MeshComponent cuboid_mesh_component{
         .index = app.Resources().GetIndex<QCE::Mesh>("cuboid")
+    };
+    QCE::MeshComponent sphere_mesh_component{
+        .index = app.Resources().GetIndex<QCE::Mesh>("sphere")
     };
     QCE::MaterialComponent textured_material_component{
         .index = app.Resources().GetIndex<QCE::Material>("textured_material")
@@ -61,6 +69,17 @@ int main(int argc, char* argv[]) {
         }));
     QCE_CRITICAL(app.m_entities.AddComponent(entity1, QCE::TransformMatrix{}));
     QCE_CRITICAL(app.m_entities.AddComponent(entity1, textured_material_component));
+
+    auto entity2 = app.m_entities.AddEntity();
+    QCE_CRITICAL(app.m_entities.AddComponent(entity2, sphere_mesh_component));
+    QCE_CRITICAL(app.m_entities.AddComponent(entity2,
+        QCE::TransformComponents{
+            { 0.0f, 0.0f, 0.0f, 1.0f },
+            { 0.0f, 0.0f, 0.0f },
+            { 1.0f, 1.0f, 1.0f }
+        }));
+    QCE_CRITICAL(app.m_entities.AddComponent(entity2, QCE::TransformMatrix{}));
+    QCE_CRITICAL(app.m_entities.AddComponent(entity2, untextured_material_component));
 
     auto sun = app.m_entities.AddEntity();
     QCE_CRITICAL(app.m_entities.AddComponent(sun,
