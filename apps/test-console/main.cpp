3a83fa03e4ed5e09b0406ef52d24d81ec0dfb5e1
JUSTSCIENCE0 <yakovmen62@gmail.com>
2026-08-03 22:49:52 +0700

    add bitsery serialization for float3d and quaternion

--- apps/test-console/main.cpp
+++ apps/test-console/main.cpp
@@ -3,53 +3,26 @@
 //
 // License: MIT
 
-#include <qce/mjson/generated_objects.hpp>
+#include <qce/binary/math.hpp>
+
+#include <vector>
 
 using namespace QCE;
 
+using Buffer = std::vector<uint8_t>;
+using OutputAdapter = bitsery::OutputBufferAdapter<Buffer>;
+using InputAdapter = bitsery::InputBufferAdapter<Buffer>;
+
 int main(int argc, char* argv[]) {
-    auto schema = macrojson::get_json_schema_str<TransformAnimation>();
-    std::cout << "TransformAnimation" << std::endl;
-    std::cout << "Generated JSON Schema:" << std::endl;
-    std::cout << schema << std::endl;
-    std::cout << std::endl;
+    float4d value = { 1.0f, 2.0f, 3.0f, 4.0f };
+    float4d deser;
 
-    QCE::TransformAnimation full_rotation{};
-    full_rotation.id = "full_rotation";
-    full_rotation.rotation_channel = {
-        {
-            /*value*/ QCE::euler_deg_to_quaternion(0.0f, 0.0f, 0.0f),
-            /*start_time*/ 0.0f
-        },
-        {
-            /*value*/ QCE::euler_deg_to_quaternion(0.0f, 90.0f, 0.0f),
-            /*start_time*/ 1.0f
-        },
-        {
-            /*value*/ QCE::euler_deg_to_quaternion(0.0f, 180.0f, 0.0f),
-            /*start_time*/ 2.0f
-        },
-        {
-            /*value*/ QCE::euler_deg_to_quaternion(0.0f, 270.0f, 0.0f),
-            /*start_time*/ 3.0f
-        },
-        {
-            /*value*/ QCE::euler_deg_to_quaternion(0.0f, 0.0f, 0.0f),
-            /*start_time*/ 4.0f
-        }
-    };
-    full_rotation.total_duration = QCE::calculate_animation_duration(full_rotation);
+    Buffer buffer;
+    auto written_bytes = bitsery::quickSerialization<OutputAdapter>(buffer, value);
 
-    std::string json{};
-    macrojson::object_to_json_str(full_rotation, json);
+    auto state = bitsery::quickDeserialization<InputAdapter>(
+        { buffer.begin(), written_bytes }, deser);
 
-    std::cout << "Serialized JSON:" << std::endl;
-    std::cout << json << std::endl;
-    std::cout << std::endl;
-
-    std::string error_descr;
-    TransformAnimation out_pos{};
-    macrojson::json_str_to_object(json, out_pos, error_descr);
-
+    assert(state.first == bitsery::ReaderError::NoError && state.second);
     return 0;
 }
---
+++ include/qce/binary/math.hpp
@@ -0,0 +1,31 @@
+// Copyright (c) 2026, Yakov Usoltsev
+// Email: yakovmen62@gmail.com
+//
+// License: MIT
+
+#pragma once
+
+#include <qce/math/math.hpp>
+
+#include <bitsery/bitsery.h>
+#include <bitsery/adapter/buffer.h>
+#include <bitsery/traits/vector.h>
+
+namespace QCE {
+    template<typename S>
+    void serialize(S& s, float3d& value) {
+        s.value4b(value.x());
+        s.value4b(value.y());
+        s.value4b(value.z());
+    }
+
+    template<typename S>
+    void serialize(S& s, quaternion& value) {
+        // TODO: Compression of quaternion
+        // 128 bit -> 32 bit (2 + 3 * 10)
+        s.value4b(value.x());
+        s.value4b(value.y());
+        s.value4b(value.z());
+        s.value4b(value.w());
+    }
+}
